7456dfaed048e651f8422dfbb8ba95b6aaf9b222
Kacie Nguyen <maidiep.nguyen@hotmail.com>
2018-03-28 09:32:19 -0400

    figured how to input data from javascript into html element.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,27 @@
-var name = prompt("What is your name?");
-confirm("Hello " + name + "." + " Let's play Hangman!");
-
 var wordBank = ["blocks", "creative", "world", "mobile", "craft"];
-var word = wordBank[Math.floor(Math.random() * wordBank.length)];
-console.log(word);
+
 
 var blank = [];
 for (var i = 0; i < word.length; i++) {
-    blank[i] = "_";
+blank[i] = "_";
 }
 
-var remainingLetters = word.length;
-while (remainingLetters > 0) {
-    alert(blank.join(" "));
-    var guess = prompt("Guess a letter.");
-    for (var j = 0; j < word.length; j++) {
-        if (guess === null) {
-            alert ("See you next time.");
-            
-        } else if 
-        (word[j] === guess) {
-            blank[j] = guess;
-            remainingLetters--;
-        }
-    }        
-}
+document.getElementById("word").innerHTML = "blank[i]";
 
-alert(blank.join(" "));
-alert("Good work! The word was " + word + ".");
+// for (var i = 0; i < word.length; i++) {
+//     var blanks = document.createElement("p");
+        
+//     var blank = [];
+//     for (var i = 0; i < word.length; i++) {
+//         blank[i] = "_";
+//     }
+
+
+
+
+
+
+
+
+
+
