227a8d5821b00d0fa7ed0eac0936f06a3ba766b2
Kacie Nguyen <maidiep.nguyen@hotmail.com>
2018-03-29 13:37:33 -0400

    added pseudo-code to help organize code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,22 @@
 var wordBank = ["blocks", "creative", "world", "mobile", "craft"];
 var word = wordBank[Math.floor(Math.random() * wordBank.length)];
+var remainingGuesses = word.length;
+
+var numSpaces = [];
+for (var i = 0; i < word.length; i++) {
+ numSpaces[i] = "_"
+}
+document.getElementById("wordContainer").innerHTML = numSpaces.join(" ");
+
+//accpeting user guess
+
+//counter for Remaining Guesses, start at 7 and decrease by 1 after each incorrect guess
+
+//update numSpaces when guess are correct
+
+//message and end game when counter reaches 0
 
 
-document.getElementById("wordContainer").innerHTML = word;
 
 
 
@@ -10,22 +24,6 @@ document.getElementById("wordContainer").innerHTML = word;
 
 
 
-
-
-// var blank = [];
-// for (var i = 0; i < word.length; i++) {
-// blank[i] = "_";
-// }
-
-// document.getElementById("word").innerHTML = "blank[i]";
-
-// for (var i = 0; i < word.length; i++) {
-//     var blanks = document.createElement("p");
-        
-//     var blank = [];
-//     for (var i = 0; i < word.length; i++) {
-//         blank[i] = "_";
-//     }
 
 
 
